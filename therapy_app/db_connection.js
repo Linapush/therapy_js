589df35da3e550361146832f333286e36c8990d5
Linapush <linapush2002@gmail.com>
2024-11-15 12:59:40 +0300

    update connection for db;

--- therapy_app/db_connection.js
+++ therapy_app/db_connection.js
@@ -1,8 +1,17 @@
 import dotenv from 'dotenv';
 import pkg from 'pg';
 
+
 dotenv.config();
 const { Pool } = pkg;
+
+const pool = new Pool({
+  user: 'linapush',
+  host: 'localhost',
+  database: 'therapy',
+  password: 'some@password123',
+  port: 5432,
+});
 
 // const pool = new Pool({
 //   user: process.env.DB_USER,
@@ -12,14 +21,9 @@ const { Pool } = pkg;
 //   port: process.env.DB_PORT,
 // });
 
-const pool = new Pool({
-  user: 'linapush',
-  host: 'localhost',
-  database: 'therapy',
-  password: 'some@password123',
-  port: 5434,
-});
 
 export default pool;
 
+
+// psql -U linapush -h localhost -d therapy -p 5432
 // console.log("Database password:", process.env.DB_PASSWORD, typeof process.env.DB_PASSWORD);
