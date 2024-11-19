b1e34a01836c05b130c5eee4b769244c1e5ca08d
Linapush <linapush2002@gmail.com>
2024-11-19 14:01:27 +0300

    update node modules;

--- therapy_app/routes/tasks.js
+++ therapy_app/routes/tasks.js
@@ -5,10 +5,10 @@ import express from 'express';
 const router = express.Router();
 
 const pool = new Pool({
-    user: 'linapush',
+    user: 'sofia',
     host: 'localhost',
     database: 'js',
-    password: '1some2hard3password4',
+    password: '1234',
     port: 5434,
 });
 
@@ -20,11 +20,10 @@ function checkAuth(req, res, next) {
     }
 }
 
-// терапевт
-router.get('/therapists', checkAuth, async (req, res) => {
+router.get('/tasks', checkAuth, async (req, res) => {
     try {
         const user = req.session.user;
-        const sessions = await pool.query('SELECT * FROM sessions WHERE user_id = (SELECT id FROM users WHERE username = $1)', [user]);
+        const tasks = await pool.query('SELECT * FROM tasks WHERE user_id = (SELECT id FROM users WHERE username = $1)', [user]);
         res.json(tasks.rows);
     } catch (err) {
         console.error(err);
@@ -32,27 +31,14 @@ router.get('/therapists', checkAuth, async (req, res) => {
     }
 });
 
-
-// сессии 
-router.get('/sessions', checkAuth, async (req, res) => {
-    try {
-        const user = req.session.user;
-        const sessions = await pool.query('SELECT * FROM sessions WHERE session_id = (SELECT id FROM sessions WHERE title = $1)', [user]);
-        res.json(sessions.rows);
-    } catch (err) {
-        console.error(err);
-        res.send('Ошибка при получении сессий.');
-    }
-});
-
-router.post('/sessions', checkAuth, async (req, res) => {
+router.post('/tasks', checkAuth, async (req, res) => {
     const { title, description } = req.body;
     const user = req.session.user;
 
     try {
         const userIdQuery = await pool.query('SELECT id FROM users WHERE username = $1', [user]);
         const userId = userIdQuery.rows[0].id;
-        await pool.query('INSERT INTO sessions (title, description, user_id) VALUES ($1, $2, $3)', [title, description, userId]);
+        await pool.query('INSERT INTO tasks (title, description, user_id) VALUES ($1, $2, $3)', [title, description, userId]);
 
         res.redirect('/dashboard');
     } catch (err) {
@@ -61,11 +47,11 @@ router.post('/sessions', checkAuth, async (req, res) => {
     }
 });
 
-router.put('/sessions/:id', checkAuth, async (req, res) => {
+router.put('/tasks/:id', checkAuth, async (req, res) => {
     const { id } = req.params;
     const { title, description, status } = req.body;
     try {
-        await pool.query('UPDATE sessions SET title = $1, description = $2, status = $3 WHERE id = $4', [title, description, status, id]);
+        await pool.query('UPDATE tasks SET title = $1, description = $2, status = $3 WHERE id = $4', [title, description, status, id]);
         res.send('Задача обновлена.');
     } catch (err) {
         console.error(err);
