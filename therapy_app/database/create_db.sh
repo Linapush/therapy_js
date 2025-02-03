4248d09289c212a69466a55086c7117426d507c9
Linapush <linapush2002@gmail.com>
2025-02-03 18:10:09 +0300

    Update configs

--- therapy_app/database/create_db.sh
+++ therapy_app/database/create_db.sh
@@ -1,3 +1,8 @@
-docker run --name therapy_js -p 8005:5432 -e POSTGRES_USER=linapush -e POSTGRES_PASSWORD=password123 -e POSTGRES_DB=therapy@ -d postgres
+docker run --name therapy_app -p 8005:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=sirius -e POSTGRES_DB=therapy -d postgres
 
-psql -h 127.0.0.1 -p 8005 -d therapy_js -U linapush -f fill_db.sql
+psql -h localhost -p 5434 -U postgres -d therapy
+
+docker volume rm therapy_app_postgres_data
+therapy_app_postgres_data
+
+docker exec -it therapy_postgres psql -U postgres -d therapy
